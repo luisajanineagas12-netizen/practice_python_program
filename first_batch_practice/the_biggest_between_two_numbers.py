baed5b8013c749a59b93b895de245d6ed3d2f317
LuisaJanineAgas <luisajanineagas12@gmail.com>
2026-03-08 05:41:20 -1100

    solve the which is the biggest between two numbers

---
+++ first_batch_practice/the_biggest_between_two_numbers.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num1 > num2:
+    print("The biggest number is: ", num1)
+else:
+    print("The biggest number is: ", num2)
