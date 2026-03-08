ce2ef8ad5bd2aa850d75e8f03c188b2a4a1087dd
LuisaJanineAgas <luisajanineagas12@gmail.com>
2026-03-08 07:17:16 -1100

    fix

--- second_batch_practice/the_smallest_number_between_two_number.py
+++ second_batch_practice/the_smallest_number_between_two_number.py
@@ -1,1 +1,6 @@
-
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+if num1 < num2:
+    print(num1, "is the smallest number")
+else:
+    print(num2, "is the smallest number")
